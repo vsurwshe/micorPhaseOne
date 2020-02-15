8c059d77ceaf5f7298aa774684f2c1a845c2085b
Narsimha_dev <snlshvsnj@gmail.com>
2020-02-15 10:25:46 +0530

    0.4.9: Implemented Fast2SMS Api in App7

--- app7/src/main/java/com/vany/SMS/service/LogService.java
+++ app7/src/main/java/com/vany/SMS/service/LogService.java
@@ -3,20 +3,18 @@ package com.vany.SMS.service;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Service;
 
-
-
 @Service
 public class LogService {
-    
-	// Setting logger object	
+
+	// Setting logger object
 	private static final Logger logger = Logger.getLogger(LogService.class);
-	
-	//------------
+
+	// ------------
 	public static void setLogger(String message) {
-			logger.error(message);
+		logger.error(message);
 	}
-	
-	//------------------
+
+	// ------------------
 	public static void setDebbug(Object object) {
 		logger.debug(object);
 	}
