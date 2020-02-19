0b8d8ebb7489d8b4f6b3b8fe3a18d09ac4ca474f
Vishvanath Surwshe <vsurwshe@gmail.com>
2020-02-19 16:41:28 +0530

    0.5.3: Resloved creation porfile error, geting profile error

--- app3/src/main/java/com/vany/securityconfig/WebSecurityConfig.java
+++ app3/src/main/java/com/vany/securityconfig/WebSecurityConfig.java
@@ -47,8 +47,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		// We don't need CSRF for this example
 		httpSecurity.csrf().disable()
 				// dont authenticate this particular request
-				.authorizeRequests().antMatchers("/", "/*.png", "/user/regsiter", "/user/token","/user/refreshToken", "/swagger**",
-						"/webjars/**", "/v2/**", "/swagger-resources/**")
+				.authorizeRequests().antMatchers("/", "/*.png", "/user/regsiter", "/user/token", "/user/refreshToken",
+						"/swagger**", "/webjars/**", "/v2/**", "/swagger-resources/**")
 				.permitAll().
 				// all other requests need to be authenticated
 				anyRequest().authenticated().and().
--- app4/src/main/java/com/vany/AuthApplications.java
+++ app4/src/main/java/com/vany/AuthApplications.java
@@ -3,9 +3,11 @@ package com.vany;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableAutoConfiguration
 @SpringBootApplication
+@EnableJpaAuditing
 public class AuthApplications {
 
 	public static void main(String[] args) {
