90a003ffd1c96d4aff8c046b62093aafb7cef24b
Vishvanath Surwshe <vsurwshe@gmail.com>
2020-02-27 15:05:38 +0530

    0.5.10: Started Address Controller

--- app4/src/main/java/com/vany/repository/AddressRepository.java
+++ app4/src/main/java/com/vany/repository/AddressRepository.java
@@ -4,14 +4,13 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import com.vany.model.Address;
 
 public interface AddressRepository extends JpaRepository<Address, Integer> {
 
 	// This method return list of Address by profile id
-	@Query(value = "select * from address  where profile_id=:profileId", nativeQuery = true)
-	List<Address> findAddressByProfileId(@Param("profileId") Integer profileId);
+	@Query(value = "select * from address  where profile_id=?1", nativeQuery = true)
+	List<Address> findAddressByProfileId(Integer profileId);
 
 }
--- app4/src/main/java/com/vany/service/ErrorServiceMessage.java
+++ app4/src/main/java/com/vany/service/ErrorServiceMessage.java
@@ -1,11 +1,12 @@
 package com.vany.service;
 
 public class ErrorServiceMessage {
-	// This is central error message handling	
+	// This is central error message handling
 	public static final String NOT_PASS_CORRECT_TOKEN = " You are passing token is not correct for these authentications. ";
 	public static final String NOT_VALID_USER = "This user, not an authorized user. This resource can access only authorized users.";
 	public static final String TOKEN_EXPIRED = " Sorry but your token time is expired";
-	//--- Profile
+	
+	// --- Profile
 	public static final String NO_REC_PROFILE = "No profile record found with id ";
 	public static final String PROFILE_UPDATE_WORNG_VERSION = " this not correct version given, the expected version is  ";
 	public static final String PROFILE_DELETE_SUCCESS = " Your profile record deleted successfully !!! ";
@@ -13,8 +14,12 @@ public class ErrorServiceMessage {
 	public static final String PROFILE_NOT_FOUND_USER = "No user record found with profile id  ";
 	public static final String PROFILE_NOT_UPDATE = "No profile record is updated successfully with id  ";
 
-	//--- Payment
+	// --- Payment
 	public static final String NO_REC_PAYMENT = "No payment records found with id ";
 	public static final String NO_REC_PROFILE_WITH_PAYMENT = " profile id related, no payment record found with payment id  ";
 	public static final String PAYMENT_DELETE_SUCCESS = " Your payment record deleted successfully !!! ";
+
+	// ---- Address
+	public static final String NO_REC_ADDRESS = "No payment records found with id ";
+	public static final String NO_REC_ADDRESS_WITH_PROFILE = "No payment records found with profile id ";
 }
