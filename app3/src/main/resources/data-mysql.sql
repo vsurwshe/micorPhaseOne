e4ef13592be59540d01140a94dcc08b091b29c39
Vishvanath Surwshe <vsurwshe@gmail.com>
2020-02-28 10:23:02 +0530

    0.5.11: Implemented the at time of creations table insert record

---
+++ app3/src/main/resources/data-mysql.sql
@@ -0,0 +1,2 @@
+INSERT INTO `user` (`user_id`, `created_at`, `is_enabled`, `name`, `updated_at`, `user_email`, `user_password`) VALUES
+(1, '2020-02-28 00:00:00.000000', b'1', 'vishva', '2020-02-28 00:00:00.000000', 'v@v.com', '123456');
---
+++ app4/src/main/resources/data-mysql.sql
@@ -0,0 +1,3 @@
+INSERT INTO `profile` (`profile_id`, `created_at`, `profile_name`, `updated_at`, `version`, `user`, `features`, `type`) VALUES
+(1, '2020-02-25 06:02:44.208000', 'Vishva', '2020-02-25 06:02:44.208000', 0, 1, 0, 0),
+(3, '2020-02-25 06:29:19.746000', 'Akash', '2020-02-25 06:29:19.746000', 0, 1, 0, 0);
