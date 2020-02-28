d4610f5958292b834deeb12329e47dd8037e8340
Vishvanath Surwshe <vsurwshe@gmail.com>
2020-02-28 15:33:07 +0530

    0.5.12: implemented Profile Types

--- app3/src/main/resources/data-mysql.sql
+++ app3/src/main/resources/data-mysql.sql
@@ -1,2 +1,4 @@
-INSERT INTO `user` (`user_id`, `created_at`, `is_enabled`, `name`, `updated_at`, `user_email`, `user_password`) VALUES
-(1, '2020-02-28 00:00:00.000000', b'1', 'vishva', '2020-02-28 00:00:00.000000', 'v@v.com', '123456');
+INSERT INTO `user` (`created_at`, `is_enabled`, `name`, `updated_at`, `user_email`, `user_password`) 
+SELECT '2020-02-28 00:00:00.000000', 1, 'vishva', '2020-02-28 00:00:00.000000', 'v@v.com', '$2y$12$glotx15sU/dgkHyNpG1zYuUE9.yYFkIiWx.j4VSrz4Cl8N2fhEiUC'
+WHERE NOT EXISTS (SELECT * FROM  `user` WHERE `user_id`=1);
+
---
+++ app4/src/main/java/com/vany/repository/ProfileTypeRepository.java
@@ -0,0 +1,10 @@
+package com.vany.repository;
+
+import com.vany.model.ProfileTypeModel;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProfileTypeRepository extends JpaRepository<ProfileTypeModel , Integer> {
+
+	
+}
