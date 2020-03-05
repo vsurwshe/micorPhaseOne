416f4000d9c3294a27049dc24220926a5d1f49e3
Narsimha_dev <snlshvsnj@gmail.com>
2020-03-05 11:52:10 +0530

    0.6.12: Added Eureka and zuul project

---
+++ zuul/src/test/java/org/zuul/ZuulApiGatewayApplicationTests.java
@@ -0,0 +1,16 @@
+package org.zuul;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ZuulApiGatewayApplicationTests.class )
+public class ZuulApiGatewayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
