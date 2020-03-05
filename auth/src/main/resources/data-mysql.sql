1b765c011a26c1a0f807f18ebf786efef35abc64
Narsimha_dev <snlshvsnj@gmail.com>
2020-03-05 15:08:10 +0530

    0.6.15: Modify the user controller in auth project

--- auth/src/main/resources/data-mysql.sql
+++ auth/src/main/resources/data-mysql.sql
@@ -1,4 +1,8 @@
 INSERT INTO `user` (`created_at`, `is_enabled`, `updated_at`, `user_balance`, `user_email`, `user_name`, `user_password`) 
-SELECT '2020-03-02 00:00:00.000000', b'0', '2020-03-02 00:00:00.000000', 0, 'v@v.com', 'vishva', '$2y$12$glotx15sU/dgkHyNpG1zYuUE9.yYFkIiWx.j4VSrz4Cl8N2fhEiUC'
+SELECT '2020-03-05 09:31:35.149000', b'0', '2020-03-05 09:31:35.149000', 0, 'm@m.com', 'akash', '$2a$10$87ZN/.okr88Ov7H7yxZJ7eYO1I3rwHslE1UATpnTXtoBcnWdFGwp2'
 WHERE NOT EXISTS (SELECT * FROM  `user` WHERE `user_id`=1);
+
+INSERT INTO `user` (`created_at`, `is_enabled`, `updated_at`, `user_balance`, `user_email`, `user_name`, `user_password`) 
+SELECT '2020-03-05 09:34:11.924000', b'0', '2020-03-05 09:34:11.924000', 0, 'v@v.com', 'vishvanath', '$2a$10$wGqni35EnbTZ9LtFM.InrO8EC83NyjN3Jc/17fOCFgZv.9zY6u09a'
+WHERE NOT EXISTS (SELECT * FROM  `user` WHERE `user_id`=2);
 
