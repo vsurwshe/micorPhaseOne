48580a8a5675e091d9a1273313f9826005761599
Narsimha_dev <snlshvsnj@gmail.com>
2020-03-05 12:51:12 +0530

    0.6.13: Modify the auth and Profile prom Files

--- eureka/src/main/java/org/eureka/EurekaServerApplication.java
+++ eureka/src/main/java/org/eureka/EurekaServerApplication.java
@@ -11,5 +11,8 @@ public class EurekaServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
 	}
+	
+	
+	
 
 }
--- profile/src/main/java/org/profile/ProfileApplication.java
+++ profile/src/main/java/org/profile/ProfileApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -17,6 +18,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 	"org.profile"
 })
 @EnableJpaRepositories("org.repository.repo")
+@EnableEurekaClient
 public class ProfileApplication {
 
 	public static void main(String[] args) {
