c46f3ffcfacf740151434ce0e76be748bde1b521
Narsimha_dev <snlshvsnj@gmail.com>
2020-03-09 10:38:40 +0530

    0.6.20: Modify the UserController in auth project

--- auth/src/main/java/org/auth/controller/UserController.java
+++ auth/src/main/java/org/auth/controller/UserController.java
@@ -10,6 +10,7 @@ import org.domain.component.UserTokenResponse;
 import org.domain.entity.UserDet;
 import org.exception.exec.UserServiceException;
 import org.service.apiService.LogService;
+import org.service.apiService.ResponseEntityResult;
 import org.service.apiService.ErrorServiceMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -19,16 +20,16 @@ import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping("/user")
 public class UserController {
-	
+
 	@Autowired
 	private AuthenticationManager authenticationManager;
 
@@ -40,57 +41,104 @@ public class UserController {
 	@Autowired
 	private JwtUserDetailsService jwtUserDetailsService;
 
-	// this method give the response with user token take username and passwrod
-	@RequestMapping(value = "/token", method = RequestMethod.POST)
-	public ResponseEntity<?> getUserToken(@Valid @RequestBody JwtRequest jwtRequest) throws Exception {
-		// Check user passed credtional are correct or not
-		authenticate(jwtRequest.getUserEmail(), jwtRequest.getUserPassword());
-		// after successfully authencticated we get user details
-		final UserDetails userDetails = jwtUserDetailsService.loadUserByUsername(jwtRequest.getUserEmail());
-		Long tokenTime = System.currentTimeMillis() + (5 * 60) * 1000;
-		// we returning above single bounded entity
-		return ResponseEntity.ok(this.setUserTokenResponse(tokenTime,userDetails));
+	// This getting User Token Form database
+	@PostMapping(value = "/token")
+	public ResponseEntity<?> findUserToken(@Valid @RequestBody JwtRequest userRequest) {
+		return this.getUserToken(userRequest);
+	}
+
+	// This method save the user into database
+	@PostMapping(value = "/register")
+	public ResponseEntity<?> saveUserDetails(@Valid @RequestBody UserDet user) {
+		return this.registerUserDetails(user);
+	}
+
+	// This method get the refresh token form database
+	@PostMapping(value = "/refreshToken")
+	public ResponseEntity<?> findRefreshToken(HttpServletRequest userRequest) {
+		return this.findRefreshToken(userRequest);
+	}
+
+	// this method give the response with user token take user name and password
+	public ResponseEntity<?> getUserToken(@Valid @RequestBody JwtRequest jwtRequest) {
+		UserTokenResponse userResult = null;
+		try {
+			// Check user passed credtional are correct or not
+			authenticate(jwtRequest.getUserEmail(), jwtRequest.getUserPassword());
+			// after successfully authenticated we get user details
+			final UserDetails userDetails = jwtUserDetailsService.loadUserByUsername(jwtRequest.getUserEmail());
+			Long tokenTime = System.currentTimeMillis() + (5 * 60) * 1000;
+			// we returning above single bounded entity
+			userResult = this.setUserTokenResponse(tokenTime, userDetails);
+		} catch (UserServiceException e) {
+			LogService.setLogger(e.getMessage());
+			return ResponseEntityResult.notFound(e.getMessage());
+		} catch (Exception e) {
+			LogService.setLogger(e.getMessage());
+			return ResponseEntityResult.badRequest(e.getMessage());
+		}
+		return ResponseEntityResult.successResponseEntity(userResult);
 	}
 
 	// this method resgiter the user in database.
-	@RequestMapping(value = "/regsiter", method = RequestMethod.POST)
 	public ResponseEntity<?> registerUserDetails(@RequestBody UserDet user) {
-		return ResponseEntity.ok(jwtUserDetailsService.save(user));
+		UserDet userResult = null;
+		try {
+			userResult = jwtUserDetailsService.save(user);
+		} catch (UserServiceException e) {
+			LogService.setLogger(e.getMessage());
+			return ResponseEntityResult.notFound(e.getMessage());
+		} catch (Exception e) {
+			LogService.setLogger(e.getMessage());
+			return ResponseEntityResult.badRequest(e.getMessage());
+		}
+		return ResponseEntityResult.successResponseEntity(userResult);
+	}
+
+	// this method take refresh token and provide to user fresh token with new
+	// refresh token
+	public ResponseEntity<?> getUserRefreshToken(HttpServletRequest userReuest) throws Exception {
+		UserTokenResponse userResult = null;
+		try {
+			// this line get refresh token form user request named as refresh
+			String refreshToken = userReuest.getHeader("refresh");
+			// this line get user name from refresh token
+			String username = jwtTokenUtil.getUsernameFromToken(refreshToken);
+			// form user name we find the user details
+			final UserDetails userDetails = jwtUserDetailsService.loadUserByUsername(username);
+			Long tokenTime = System.currentTimeMillis() + (5 * 60 * 60) * 1000;
+			// we returning above single bounded entity
+			userResult = this.setUserTokenResponse(tokenTime, userDetails);
+		} catch (UserServiceException e) {
+			LogService.setLogger(e.getMessage());
+			return ResponseEntityResult.notFound(e.getMessage());
+		} catch (Exception e) {
+			LogService.setLogger(e.getMessage());
+			return ResponseEntityResult.badRequest(e.getMessage());
+		}
+		return ResponseEntityResult.successResponseEntity(userResult);
 	}
 
 	private void authenticate(String username, String password) throws Exception {
 		try {
 			authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
 		} catch (DisabledException e) {
-			LogService.setLogger(ErrorServiceMessage.USER_LOCKED+" "+e.getMessage());
+			LogService.setLogger(ErrorServiceMessage.USER_LOCKED + " " + e.getMessage());
 			throw new UserServiceException(ErrorServiceMessage.USER_LOCKED + e.getMessage());
 		} catch (BadCredentialsException e) {
-			LogService.setLogger(ErrorServiceMessage.INVALID_CREDTIONAL+" "+e.getMessage());
+			LogService.setLogger(ErrorServiceMessage.INVALID_CREDTIONAL + " " + e.getMessage());
 			throw new Exception(ErrorServiceMessage.INVALID_CREDTIONAL + e.getMessage());
 		}
 	}
 
-	// this method take refresh token and provide to user fresh token with new
-	// refresh token
-	@RequestMapping(value = "/refreshToken", method = RequestMethod.POST)
-	public ResponseEntity<?> getUserRefreshToken(HttpServletRequest userReuest) throws Exception {
-		// this line get refresh token form user request named as refresh
-		String refreshToken = userReuest.getHeader("refresh");
-		// this line get username from refresh token
-		String username = jwtTokenUtil.getUsernameFromToken(refreshToken);
-		// form username we find the user details
-		final UserDetails userDetails = jwtUserDetailsService.loadUserByUsername(username);
-		Long tokenTime = System.currentTimeMillis() + (5 * 60 * 60 ) * 1000;
-		// we returning above single bounded entity
-		return ResponseEntity.ok(this.setUserTokenResponse(tokenTime,userDetails));
-	}
-	//---------- Private Methods
+	// ---------- Private Methods
 	private UserTokenResponse setUserTokenResponse(Long tokenTime, UserDetails userDetails) throws Exception {
-		UserTokenResponse userTokenResponse=null;
+		UserTokenResponse userTokenResponse = null;
 		try {
-			// after geting user token, we try to genrate token for geting new user
+			// after getting user token, we try to generate token for getting new user
 			final String userToken = jwtTokenUtil.generateToken(userDetails);
-			// after geting user token, we try to genrate refresh token for geting new user token
+			// after getting user token, we try to generate refresh token for getting new
+			// user token
 			final String userRefreshToken = jwtTokenUtil.genrateRefreshToken(userDetails);
 			userTokenResponse = new UserTokenResponse();
 			userTokenResponse.setTokenExprieTime(tokenTime);
@@ -98,7 +146,7 @@ public class UserController {
 			userTokenResponse.setUserRefreshToken(userRefreshToken);
 		} catch (Exception e) {
 			LogService.setLogger(e.getMessage());
-			throw new Exception(ErrorServiceMessage.NOT_VALID_USER+" "+e.getMessage());
+			throw new Exception(ErrorServiceMessage.NOT_VALID_USER + " " + e.getMessage());
 		}
 		return userTokenResponse;
 	}
